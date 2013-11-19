8ed2814b0306539570ef7e738ce995bee58c0c35
Yuval Langer <yuval.langer@gmail.com>
2013-11-19 21:43:45 +0200

    Bottles

--- src/main.py
+++ src/main.py
@@ -8,6 +8,23 @@ from bottle import route, default_app, run
 def hello(somesuch):
     return "Hello from bottle! :-)" + "<br /> " + somesuch
 
+def bottles(n):
+    return [
+        sentence
+        for i in range(n, -1, -1)
+        for sentence in [
+            '{bottlenum} bottles of beer on the wall, {bottlenum} bottles of beer.'.format(bottlenum=n),
+            'Take one down, pass it around, {bottlenum} bottles of beer on the wall...'.format(bottlenum=n-1),
+        ]
+    ] + [
+        "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.",
+    ]
+
+@route('/bottles/<n:int>')
+def bottle_bottles(n):
+    return '<br />'.join(bottles(n))
+    
+
 application = default_app()
 
 if __name__ == '__main__':
