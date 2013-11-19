37cfbeb8f98c948afd63006abf2c6c3a78416ec1
Yuval Langer <yuval.langer@gmail.com>
2013-11-19 22:19:49 +0200

    Adding the n-bottles form.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 Hackita lessons
 '''
 
-from bottle import route, default_app, run
+from bottle import route, default_app, run, get, post, request
 
 @route('/<somesuch>')
 def hello(somesuch):
@@ -11,18 +11,37 @@ def hello(somesuch):
 def bottles(n):
     return [
         sentence
-        for i in range(n, -1, -1)
+        for i in range(n, 0, -1)
         for sentence in [
-            '{bottlenum} bottles of beer on the wall, {bottlenum} bottles of beer.'.format(bottlenum=n),
-            'Take one down, pass it around, {bottlenum} bottles of beer on the wall...'.format(bottlenum=n-1),
+            '{bottlenum} bottles of beer on the wall, {bottlenum} bottles of beer.'.format(bottlenum=i),
+            'Take one down, pass it around, {bottlenum} bottles of beer on the wall...'.format(bottlenum=i-1),
+            '<br />',
         ]
     ] + [
         "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.",
     ]
 
-@route('/bottles/<n:int>')
-def bottle_bottles(n):
+def bottles_html(n):
     return '<br />'.join(bottles(n))
+
+@get('/bottles')
+@get('/bottles/')
+def bottle_get():
+    return '''
+    <br /><br />
+        <form action="/bottles" method="post">
+            Number of bottles: <input name="n" type="number" />
+            <input value="Break some bottles" type="submit" />
+        </form>
+    '''
+    
+@get('/bottles/<n:int>')
+def bottle_n(n):
+    return bottles_html(n)
+
+@post('/bottles')
+def bottle_bottles():
+    return bottles_html(int(request.forms.get('n'))) + bottle_get()
     
 
 application = default_app()
