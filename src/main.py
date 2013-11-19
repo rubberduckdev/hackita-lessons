6c275a6d66c4e76aec44310572c63cd0259ab68c
Yuval Langer <yuval.langer@gmail.com>
2013-11-19 17:33:47 +0200

    First commit

---
+++ src/main.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python2.7
+# encoding: utf-8
+
+'''
+Hackita lessons
+'''
+
+from bottle import route, default_app, run
+
+@route('/')
+def get(self):
+    return "Hello from bottle! :-)"
+
+application = default_app()
+
+if __name__ == '__main__':
+    run(host='localhost', port=8080, reloader=True)
