c2981217d7371894bbdbb2a5d9c299bf632716bd
Yuval Langer <yuval.langer@gmail.com>
2013-11-19 18:35:53 +0200

    I don't know if I can use __name__ = '__main__', but I add it anyways.

--- main.py
+++ main.py
@@ -7,8 +7,15 @@ Hackita lessons
 
 from bottle import route, default_app, run
 
-@route('/')
-def hello():
-    return "Hello from bottle! :-)"
+@route('/<somesuch>')
+def hello(somesuch):
+    return "Hello from bottle! :-)" + "<br /> " + somesuch
 
 application = default_app()
+
+if __name__ == '__main__':
+    run(
+        host='localhost',
+        port=8080,
+        reloader=True,
+    )
